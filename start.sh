d7489cce2a912ddee642aca5f1a9fe44dba184c9
root <root@h2856191.stratoserver.net>
2021-10-11 16:58:55 +0200

    commit message

---
+++ start.sh
@@ -0,0 +1,2 @@
+#sudo /home/captain/.linuxbrew/bin/hugo server --baseURL=http://captaincrunch.nl/ --bind=0.0.0.0 --port=8080
+/home/captain/.linuxbrew/bin/hugo -c /var/www/hugo/ -d /var/www/captaincrunch.nl/
